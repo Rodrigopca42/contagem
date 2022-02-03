94c106d460c0e8c406753d26ca43c0f746c7ba2a
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-02-03 17:57:07 -0300

    readme
    readme

---
+++ javascript/folha.js
@@ -0,0 +1,8 @@
+function contar(){
+var tn1 = document.querySelector('#txtn1');
+var tn2 = document.querySelector('#txtn2');
+var tn3 = document.querySelector('#txtn3');
+var res = document.querySelector('#res');
+
+if ( )
+}
